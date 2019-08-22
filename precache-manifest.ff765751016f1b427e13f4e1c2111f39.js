6730265fb69ea4b5218334a7fa6273ca156b17b6
Rohan Mahajan <rohan.mahajan@gartner.com>
2019-08-22 13:32:49 +0530

    Updates

---
+++ precache-manifest.ff765751016f1b427e13f4e1c2111f39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba51fcea454ec196ed65148566a412d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b332340c3227761e02ef",
+    "url": "/static/css/main.3375347c.chunk.css"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "b332340c3227761e02ef",
+    "url": "/static/js/main.79b6327f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02ab761fb778bd0acea4a47a33a93617.js"
+  "/precache-manifest.ff765751016f1b427e13f4e1c2111f39.js"
 );
 
 self.addEventListener('message', (event) => {
