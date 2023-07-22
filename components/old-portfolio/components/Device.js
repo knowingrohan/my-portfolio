dcc7cd4889daf71c97e409fc56f15da6137db1f1
rohan mahajan <rohanm789@gmail.com>
2023-07-22 06:08:55 +0530

    structure and directory changes

---
+++ components/old-portfolio/components/Device.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import LaptopImg from '../assets/mac1.png';
+
+// const background = {
+//     backgroundImage: `url(${LaptopImg})`
+// }
+// const Device = (props) => {
+//     return (
+//         <div className="device__container" style={background}>
+//             <div className="device__window">
+//                 {props.children}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Device;
