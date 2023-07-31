7b40c71350d75c9067d8ce8bd55ead1814b5dbef
rohan mahajan <rohanm789@gmail.com>
2023-08-01 00:36:02 +0530

    SEO component modified

--- components/seo.js
+++ components/seo.js
@@ -7,23 +7,21 @@ const SEO = ({ title, description, image, keywords, link }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
       <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta name="url" content={`https://rohanmahajan.in${link}`} />
       <meta name="author" content="Rohan Mahajan" />
       <meta name="robots" content="index, follow" />
       <meta name="keywords" content={keywords} />
+      <meta name="type" content="website" />
+
       {/* Open Graph (OG) meta tags */}
-      <meta name="title" property="og:title" content={title} />
-      <meta
-        name="description"
-        property="og:description"
-        content={description}
-      />
-      <meta name="type" property="og:type" content="website" />
-      <meta
-        name="url"
-        property="og:url"
-        content={`https://rohanmahajan.in${link}`}
-      />
-      <meta name="image" property="og:image" content={image} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={`https://rohanmahajan.in${link}`} />
+      <meta property="og:image" content={image} />
+      <meta property="og:type" content="website" />
     </Head>
   );
 };
